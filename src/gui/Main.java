512c9cebc1850558af6e04051200606def8bb569
coolcute <coolcute@7c0ca99a-d064-11de-83be-cfe0592a616e>
2009-11-15 16:00:24 +0000

    init

---
+++ src/gui/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gui;
+
+/**
+ * @author Cool
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
