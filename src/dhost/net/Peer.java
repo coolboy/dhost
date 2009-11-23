6c82766129bf8530d0999cec5f52ffea509c0aa8
dev@openserve.org <dev@openserve.org@7c0ca99a-d064-11de-83be-cfe0592a616e>
2009-11-23 05:40:44 +0000

    Peer.java: trivial code-style cleanups (validating SVN functionality)

--- src/dhost/net/Peer.java
+++ src/dhost/net/Peer.java
@@ -2,9 +2,8 @@ package dhost.net;
 
 import java.net.*;
 
-
-public class Peer implements Comparable<Peer>{
-
+public class Peer implements Comparable<Peer>
+{
 	private static final int defaultPort = 44777;
 	private Integer peerID;
 	private int portNumber;
@@ -12,54 +11,61 @@ public class Peer implements Comparable<Peer>{
 	//variable for bandwidth score? latency score? hardware score?
 
 	//construct peer on just an IP address, in string form
-	public Peer(String peerIPAddr) throws UnknownHostException{
-
-		InetAddr =  InetAddress.getByName(peerIPAddr);
+	public Peer(String peerIPAddr) throws UnknownHostException
+	{
+		InetAddr = InetAddress.getByName(peerIPAddr);
 		portNumber = defaultPort;
-		peerID =new Integer(-1);
+		peerID = new Integer(-1);
 	}
 
 	//construct on IP, ID...
-	public Peer(String peerIPAddr, int id) throws UnknownHostException{
-		InetAddr =  InetAddress.getByName(peerIPAddr);
+	public Peer(String peerIPAddr, int id) throws UnknownHostException
+	{
+		InetAddr = InetAddress.getByName(peerIPAddr);
 		peerID = new Integer(id);
 		portNumber = defaultPort;
-
 	}
 
 	//construct on IP, port, and ID
-	public Peer(String peerIPAddr, int id, int port) throws UnknownHostException{
-		InetAddr =  InetAddress.getByName(peerIPAddr);
+	public Peer(String peerIPAddr, int id, int port) throws UnknownHostException
+	{
+		InetAddr = InetAddress.getByName(peerIPAddr);
 		portNumber = port;
 		peerID = new Integer(id);
 	}
 
-	public void setPort(int port){
-		portNumber =port;
+	public void setPort(int port)
+	{
+		portNumber = port;
 	}
 
-	public void setID(int i){
+	public void setID(int i)
+	{
 		peerID = i;
-
 	}
 
-	public int getID(){
+	public int getID()
+	{
 		return peerID;
 	}
-	public int getPort(){
+	
+	public int getPort()
+	{
 		return portNumber;
 	}
-	public InetAddress getAddr(){
+	
+	public InetAddress getAddr()
+	{
 		return InetAddr;
 	}
 
-	public int compareTo(Peer anotherPeer){
-	    
+	public int compareTo(Peer anotherPeer)
+	{    
 		int anotherPeerID = anotherPeer.getID();
-		if(this.peerID<anotherPeerID) return -1;
-		else if(this.peerID > anotherPeerID) return 1;
+		if (this.peerID<anotherPeerID)
+			return -1;
+		else if (this.peerID > anotherPeerID)
+			return 1;
 		else return 0;
 	}
-
-
 }
